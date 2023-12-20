9b6ec35d1831da012d4770f38b5cb9eb9a6f9563
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2023-12-20 13:57:55 +0900

    GetMapping, PostMapping, RequestMapping 설명하였으나 따라하기 실패 - MainController::showMain()

--- src/main/java/me/banggusuk/sbb/controller/MainController.java
+++ src/main/java/me/banggusuk/sbb/controller/MainController.java
@@ -1,6 +1,7 @@
 package me.banggusuk.sbb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,5 +19,19 @@ public class MainController {
   public String index() {
 
     return "안녕하세요";
+  }
+
+  /**
+   * 11강 2023-12-20
+   * return 값이 html로 넘어가지 않음?? -> 이렇게는 사용하지 않으니까 그리 신경쓰지 말자!!!
+   * @return
+   */
+  @GetMapping(value = "/test")
+  public String showMain() {
+
+    return """
+        <h1>안녕하세요</h1>
+        <input type="text" placeholder="입력해주세요." />
+        """;
   }
 }
