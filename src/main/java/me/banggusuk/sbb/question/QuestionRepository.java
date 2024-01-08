89164a21f91d9059bb1acff9ee14d7dc2b67a1fc
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2024-01-08 16:54:03 +0900

    41강 - testJpa2() : findBySubject() 테스트

--- src/main/java/me/banggusuk/sbb/question/QuestionRepository.java
+++ src/main/java/me/banggusuk/sbb/question/QuestionRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * date   : 2024-01-03
  */
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+  Question findBySubject(String subject);
 }
--- src/test/java/me/banggusuk/sbb/SbbApplicationTests.java
+++ src/test/java/me/banggusuk/sbb/SbbApplicationTests.java
@@ -23,7 +23,7 @@ class SbbApplicationTests {
 
     // 첫 번째 질문
     Question q1 = new Question();
-    q1.setSubject( "sbb가 무엇인강요?" );
+    q1.setSubject( "sbb가 무엇인가요?" );
     q1.setContent( "sbb에 대해서 알고 싶습니다." );
     q1.setCreateDate( LocalDateTime.now() );
     this.questionRepository.save( q1 );
@@ -48,7 +48,22 @@ class SbbApplicationTests {
     assertEquals( 2, all.size() );
 
     Question q = all.get( 0 );
-    assertEquals( "sbb가 무엇인강요?", q.getSubject() );
+    assertEquals( "sbb가 무엇인가요?", q.getSubject() );
   }
 
+
+  @Test
+  void testJpa3() {
+
+    Question q = questionRepository.findBySubject( "sbb가 무엇인가요?" );
+
+    /*
+    Optional<Question> oq = this.questionRepository.findById( 1 );
+    if (oq.isPresent()) {
+
+      Question q = oq.get();
+      assertEquals( "sbb가 무엇인강요?", q.getSubject() );
+    }
+     */
+  }
 }
